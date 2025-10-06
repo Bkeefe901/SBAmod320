316f44c9dc79cdf319e5ed99a65e35f0d77487bb
Bkeefe901 <bradyjokeefe@gmail.com>
2025-10-06 16:11:15 -0400

    Basic react setup, with react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+
+// Pages
+import NavBar from './components/NavBar';
+
+
+// Components
+
+function App() {
+
+
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </>
+  )
+}
+
+export default App
